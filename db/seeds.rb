58bf8212df3e2726261a6295aa5302dfffb414b4
Leo Walters <leoswalters@gmail.com>
2015-08-28 15:33:58 -0700

    Add check for unique records

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,25 +2,38 @@
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 
 
-require 'faker'
+  require 'faker'
 
-50.times do
-  Post.create!(
-    title: Faker::Lorem.sentence,
-    body: Faker::Lorem.paragraph
-    )
-end
+  # 50.times do
+  #   Post.create!(
+  #     title: Faker::Lorem.sentence,
+  #     body: Faker::Lorem.paragraph
+  #     )
+  # end
 
-posts = Post.all
+  # posts = Post.all
 
-100.times do
-  Comment.create!(
-    post: posts.sample,
-    body: Faker::Lorem.paragraph
-    )
-end
+  # 100.times do
+  #   Comment.create!(
+  #     post: posts.sample,
+  #     body: Faker::Lorem.paragraph
+  #     )
+  # end
 
-puts "Seed finished"
-puts "#{Post.count} posts created"
-puts "#{Comment.count} comments created"
+  # unique_post = Post.create!({title: "The Count of Monte Cristo", body: "This is such an amazing book!!!!"})
+
+  # unique_post.comments.create!({body: "I also loved this book, so good!"})
+
+  #if Post.find_by title: "The Hearts Code"
+  if Post.uniq(title: "The Hearts Code")
+    puts "That's already in the database!"
+  else
+    unique_post_3 = Post.create!({title: "Be Here Now", body: "A metaphysical classic!"})
+
+    unique_post_3.comments.create!({body: "Yeah, Ram Dass is cool."})
+  end
+
+  puts "Seed finished"
+  puts "#{Post.count} posts created"
+  puts "#{Comment.count} comments created"
 
