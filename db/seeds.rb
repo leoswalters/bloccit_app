56c4837f10e966f85822406a935d068912db9f9a
Leo Walters <leoswalters@gmail.com>
2015-09-03 10:38:13 -0700

    Add user to post associations

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,8 +4,23 @@
 
 require 'faker'
 
+#Create Users
+5.times do
+  user = User.new(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    password: Faker::Lorem.characters(10)
+  )
+
+  user.skip_confirmation!
+  user.save!
+end
+
+users = User.all
+
 50.times do
   Post.create!(
+    user: users.sample,
     title: Faker::Lorem.sentence,
     body: Faker::Lorem.paragraph
     )
@@ -15,10 +30,18 @@ posts = Post.all
 
 100.times do
   Comment.create!(
+    #user: users.sample,
     post: posts.sample,
     body: Faker::Lorem.paragraph
     )
 end
+
+user = User.first
+user.skip_reconfirmation!
+user.update_attributes!(
+  email: 'youremail.com',
+  password: 'helloworld'
+)
 
 puts "Seed finished"
 puts "#{Post.count} posts created"
