5085ac99cfd6fd237a8bc6846c9cb8abb244677e
Leo Walters <leoswalters@gmail.com>
2015-09-07 12:57:15 -0700

    Finalize user authorizations.

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -1,22 +1,19 @@
 class PostPolicy < ApplicationPolicy
+  # user => current_user
+#   scope => Post
   def index?
     true
   end
 
   class Scope < Scope
     def resolve
-      if user.admin?
+      if user.admin? || user.moderator?
         scope.all
-      elsif user.moderator?
-        scope.all
-      #elsif user.member?
-       # scope.where(record.user) #this may not be correct
-      #else
-        #scope.where(:published => true)
+      elsif user.member?
+       scope.where(user_id: user.id)
+      elsif
+        scope.none
       end
     end
-
-
   end
-
 end
