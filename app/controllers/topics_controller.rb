b035e32c4ccdf2c77a1d68fa2776237c84db999f
Leo Walters <leoswalters@gmail.com>
2015-09-07 11:24:24 -0700

    Add topic functionality

---
+++ app/controllers/topics_controller.rb
@@ -0,0 +1,46 @@
+class TopicsController < ApplicationController
+  def index
+    @topics = Topic.all
+    authorize @topics
+  end
+
+  def new
+    @topic = Topic.new
+    authorize @topic
+  end
+
+  def show
+    @topic = Topic.find(params[:id])
+    @posts = @topic.posts
+    authorize @topic
+  end
+
+  def edit
+    @topic = Topic.find(params[:id])
+    authorize @topic
+  end
+
+  def create
+    @topic = Topic.new(params.require(:topic).permit(:name, :description, :public))
+    authorize @topic
+    if @topic.save
+      flash[:notice] = "Topic created"
+      redirect_to @topic
+    else
+      flash[:error] = "Woopsie Daisy. Something went wrong."
+      render :new
+    end
+  end
+
+  def update
+    @topic = Topic.find(params[:id])
+    authorize @topic
+    if @topic.update_attributes(params.require(:topic).permit(:name, :description, :public))
+      flash[:notice] = "Success!"
+      redirect_to @topic
+    else
+      flash[:error] = "Something went awry. Give it another go!"
+      render :edit
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -18,9 +18,21 @@ end
 
 users = User.all
 
+#Create Topics
+15.times do
+  Topic.create!(
+    name: Faker::Lorem.sentence,
+    description: Faker::Lorem.paragraph
+    )
+end
+topics = Topic.all
+
+
+
 50.times do
   Post.create!(
     user: users.sample,
+    topic: topics.sample,
     title: Faker::Lorem.sentence,
     body: Faker::Lorem.paragraph
     )
