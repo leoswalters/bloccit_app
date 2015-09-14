acc7fc0f259dbb56eb6b05f79a9cb20a3934cf07
Leo Walters <leoswalters@gmail.com>
2015-09-14 12:18:38 -0700

    Correct error.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -37,7 +37,7 @@ class PostsController < ApplicationController
     @topic = Topic.find(params[:topic_id])
     @post = Post.find(params[:id])
     authorize @post
-    if @post.update_attributes(post_params))
+    if @post.update_attributes(post_params)
       flash[:notice] = "Post was updated."
       redirect_to [@topic, @post]
     else
