016d2bd879fc1bc7d14064e6d0a1696a6e38c5bb
Leo Walters <leoswalters@gmail.com>
2015-09-14 12:21:07 -0700

    Fixing error.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,7 +14,7 @@ class PostsController < ApplicationController
 
   def create
     @topic = Topic.find(params[:topic_id])
-    @post = Post.new(post_params))
+    @post = Post.new(post_params)
     @post.user = current_user
     @post.topic = @topic
     authorize @post
